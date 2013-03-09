6aea70a99fbf7b872d9f5ffa92fb5949fb9030ab
Andrew Austin <andrewaclt@gmail.com>
2013-03-09 15:57:36 -0500

    1) Represent the map using an nxn array. 2) First attempt at tile picking. It doesn't work.

--- js/iso.js
+++ js/iso.js
@@ -10,25 +10,52 @@ window.onload = function() {
     dirt.src = "images/dirt.png";
     tree.src = "images/tree.png";
 
+    var map = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+
     draw();
+
+    canvas.addEventListener('click', handleMouseClick, false);
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        for(var y = 0; y < 100; y++) {
+        for(var y = 0; y < map.length; y++) {
             var xOffset = 0;
             if (y % 2 != 0) {
                 xOffset = grass.width / 2;
             }
 
-            for(var x = 0; x < 100; x++) {
+            for(var x = 0; x < map[0].length; x++) {
                 var tilePosX = (x * grass.width) + xOffset;
                 var tilePosY = (y * grass.height) / 2;
 
-                ctx.drawImage(grass, Math.round(tilePosX), Math.round(tilePosY), grass.width, grass.height);
+                if(map[y][x] == 0) {
+                    ctx.drawImage(grass, Math.round(tilePosX), Math.round(tilePosY), grass.width, grass.height);
+                } else {
+                    ctx.drawImage(dirt, Math.round(tilePosX), Math.round(tilePosY), grass.width, grass.height);
+                }
             }
 
         }
 
+    }
+
+    function handleMouseClick(e) {
+        e.preventDefault();
+
+        console.log('X: ' + e.clientX + ' Y:' + e.clientY);
+
+        var tileX = Math.round(e.clientX / grass.width);
+        var tileY = Math.round(e.clientY / grass.height);
+
+        map[tileY][tileX] = 1;
+        console.log('(' + tileX + ', ' + tileY + ')');
+        draw();
     }
 }
